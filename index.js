0ec33c491d14201248faac4df8f046e325328045
User Name <artur.it2914@gmail.com>
2020-07-08 15:29:48 +0300

    Add try-catch example

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 'use strict';
+
+/**
+ *
+ * @param {number} base
+ * @param {number} exponent
+ * @returns {number}
+ */
+const pow = (base, exponent) => {
+
+  if (isNaN( base ) || isNaN( exponent )) {
+    return NaN;
+  }
+
+  if (typeof base !== 'number' || typeof exponent !== 'number') {
+    throw new TypeError( 'params base and exponent must be an numbers' );
+  }
+
+  if (!isFinite( base ) || !isFinite( exponent )) {
+    throw new RangeError();
+  }
+
+  if (exponent === 0) {
+    return 1;
+  }
+
+  if (exponent < 0) {
+    return 1 / pow( base, Math.abs( exponent ) );
+  }
+
+  return base * pow( base, exponent - 1 );
+
+};
+
+console.log( 'before' );
+try {
+  console.log( pow( '2', 3 ) );
+} catch (e) {
+  console.error( e );
+}
+console.log( 'after' );
